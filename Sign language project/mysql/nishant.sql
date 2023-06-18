de8358d03e4f60e964b77d0e6ca0c7199514a2ea
Ankit yadav <79680901+EvilAnkityadav@users.noreply.github.com>
2023-06-18 17:42:58 +0530

    Add files via upload

---
+++ Sign language project/mysql/nishant.sql
@@ -0,0 +1,14 @@
+-- Create the STATION table
+CREATE TABLE STATION (
+  city VARCHAR(50),
+  id INT AUTO_INCREMENT PRIMARY KEY
+);
+
+-- Insert random values into the table
+INSERT INTO STATION (city) VALUES ('New York');
+INSERT INTO STATION (city) VALUES ('Los Angeles');
+INSERT INTO STATION (city) VALUES ('Chicago');
+INSERT INTO STATION (city) VALUES ('Houston');
+INSERT INTO STATION (city) VALUES ('Miami');
+INSERT INTO STATION (city) VALUES ('San Francisco');
+INSERT INTO STATION (city) VALUES ('Seattle');
